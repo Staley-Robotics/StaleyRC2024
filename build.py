f36e1ecbee9465f922198409f7f259f0ef7c12f8
trindels <36929500+trindels@users.noreply.github.com>
2024-01-24 14:05:58 -0600

    Updated Subsystem Architecture

--- build.py
+++ build.py
@@ -1,16 +1,22 @@
 import os
 from pathlib import Path
 
+def scandir( dir, recursionText="" ) -> str:
+    text = """"""
+    
+    for e1 in os.scandir( dir ):
+        print(e1.path)
+        if e1.is_dir():
+            text += scandir( e1, f"{recursionText}.{e1.name}")
+        elif e1.name.endswith(".py") and e1.name != "__init__.py":
+            f = e1.name.removesuffix(".py")
+            text += f"from {recursionText}.{f} import *\n"
+        else:
+            pass
+     
+    return text
+
 for p in [ 'autonomous', 'commands', 'sequences', 'subsystems', 'util' ]:
     dir = Path( p )
-
-    text = """"""
-    for file in os.listdir( dir ):
-        
-        if file.endswith(".py") and file != "__init__.py":
-            f = file.removesuffix(".py")
-            text += f"from .{f} import *\n"
-
-        (dir / '__init__.py').write_text(
-            text
-        )
+    text = scandir( dir )
+    (dir / '__init__.py').write_text( text )
