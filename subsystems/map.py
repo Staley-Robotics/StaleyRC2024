87f09b7ba4cad622fb2fa8906818a762a724498c
Cameron Cooper <coopercc06@gmail.com>
2024-01-24 13:00:23 -0600

    map.py completed

--- subsystems/map.py
+++ subsystems/map.py
@@ -1,3 +1,6 @@
+# MANY VALUES ARE INCORRECT
+# Y values are offset on the highest border points, X values are offset nayways but flat out wrong on farthest right
+
 x = {
     # fieldEdges considers right and left to be relative to ally side.
     # All 'right' and 'left' valeus are based off of driver view.
@@ -11,27 +14,59 @@ x = {
         "oppRightFar":   16.358,
         "oppLeft":       16.358
     },
-    "Speaker": {
-        "frontCenter": 2.006,
-        "frontRight":  2.006,
-        "backRight":   1.131,
-        "frontLeft":   2.006,
-        "backRight":   1.131,
+    "Speakers": {
+        "AllySpeaker": {
+            "frontCenter": 2.006,
+            "frontRight":  2.006,
+            "backRight":   1.131,
+            "frontLeft":   2.006,
+            "backRight":   1.131
+        },
+        "OppSpeaker": {
+            "frontCenter": 15.969,
+            "frontRight":  15.969,
+            "backRight":   16.391,
+            "frontLeft":   15.969,
+            "backRight":   16.391
+        }
     },
 
     "Stage": {
         "AllyLeft": {
-            "far": 6.855
+            "far": 6.855,
+            "left": 6.353,
+            "right": 6.52,
+            "close": 6.044
         },
         "AllyCenter": {
             "farLeft": 4.617,
             "farRight": 4.617,
-            "closeLeft": 4.617,
-            "farLeft": 4.617
+            "closeLeft": 4.034,
+            "closeRight": 4.034
         },
         "AllyRight": {
-            "far": 6.855
-            
+            "far": 6.855,
+            "left": 6.52,
+            "right": 6.353,
+            "close": 6.044
+        },
+        "OppLeft": {
+            "close": 11.072,
+            "left": 11.639,
+            "right": 11.38,
+            "far": 11.915
+        },
+        "OppCenter": {
+            "leftFar": 13.926,
+            "rightFar": 13.926,
+            "leftClose": 13.375,
+            "rightClose": 13.375
+        },
+        "OppRight": {
+            "close": 11.072,
+            "right": 11.639,
+            "left": 11.38,
+            "far": 11.915
         }
     },
 
@@ -55,5 +90,85 @@ x = {
 }
 
 y = {
+    "fieldEdges": {
+        "allyRightClose": 1.363,
+        "allyRightFar":   0.309,
+        "allyLeft":       8.012,
 
+        "oppRightClose": 0.309,
+        "oppRightFar":   1.363,
+        "oppLeft":       8.012
+    },
+    "Speakers": {
+        "AllySpeaker": {
+            "frontCenter": 5.612,
+            "frontRight":  5.518,
+            "backRight":   4.590,
+            "frontLeft":   6.131,
+            "backRight":   4.639
+        },
+        "OppSpeaker": {
+            "frontCenter": 5.612,
+            "frontRight":  5.518,
+            "backRight":   4.590,
+            "frontLeft":   6.131,
+            "backRight":   4.639
+        }
+    },
+
+    "Stage": {
+        "AllyLeft": {
+            "far": 5.596,
+            "left": 5.904,
+            "right": 5.077,
+            "close": 5.385
+        },
+        "AllyCenter": {
+            "farLeft": 4.558,
+            "farRight": 3.974,
+            "closeLeft": 4.558,
+            "closeRight": 3.974 
+        },
+        "AllyRight": {
+            "far": 2.904,
+            "left": 3.423,
+            "right": 2.644,
+            "close": 3.131
+        },
+        "OppLeft": {
+            "close": 5.596,
+            "left": 5.904,
+            "right": 5.077,
+            "far": 5.385
+        },
+        "OppCenter": {
+            "leftFar": 4.558,
+            "rightFar": 3.974,
+            "leftClose": 4.558,
+            "rightClose": 3.974
+        },
+        "OppRight": {
+            "close": 2.904,
+            "right": 2.644,
+            "left": 3.423,
+            "far": 3.131
+        }
+    },
+
+
+    "Tori": {
+        "centerLL": 7.461,
+        "centerL": 5.855,
+        "centerC": 4.266,
+        "centerR": 2.644,
+        "centerRR": 1.055,
+
+        "allyL": 7.007,
+        "allyC": 5.645,
+        "allyR": 4.250,
+
+        "oppL": 7.007,
+        "oppC": 5.645,
+        "oppR": 4.250
+    }
 }
