3c76645705a56c104825c51fff18c33f7c195deb
trindels <36929500+trindels@users.noreply.github.com>
2024-03-16 09:42:01 -0500

    Adjusted Launcher

--- subsystems/Launcher/Launcher.py
+++ subsystems/Launcher/Launcher.py
@@ -7,8 +7,8 @@ from util import *
 
 class Launcher(Subsystem):
     class LauncherSpeeds:
-        SpeakerLeft = NTTunableFloat( "/Config/LauncherSpeeds/Speaker/Left", 12000.0, persistent=True )
-        SpeakerRight = NTTunableFloat( "/Config/LauncherSpeeds/Speaker/Right", 10000.0, persistent=True )
+        SpeakerLeft = NTTunableFloat( "/Config/LauncherSpeeds/Speaker/Left", 14500.0, persistent=True )
+        SpeakerRight = NTTunableFloat( "/Config/LauncherSpeeds/Speaker/Right", 12000.0, persistent=True )
         AmpLeft = NTTunableFloat( "/Config/LauncherSpeeds/Amp/Left", 2000.0, persistent=True )
         AmpRight = NTTunableFloat( "/Config/LauncherSpeeds/Amp/Right", 2000.0, persistent=True )
         TrapLeft = NTTunableFloat( "/Config/LauncherSpeeds/Trap/Left", 8000.0, persistent=True )
